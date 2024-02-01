1441c71b08c76bfa5b272d77d39936bbe795d5d3
Thach.Le <thachln@mks.com.vn>
2024-02-01 22:31:39 +0700

    Prepare libraries to predict mosquitos in project https://github.com/thachln/Hawkeye.

--- 01_install.cmd
+++ 01_install.cmd
@@ -4,3 +4,4 @@ python -m venv venv
 call venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
