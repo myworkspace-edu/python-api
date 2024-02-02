f93b63c64ef7932a5242489607f81f7a07f4cac7
Thach.Le <thachln@mks.com.vn>
2024-02-02 07:16:55 +0700

    Use CALL statement to execute a script or command.

--- 01_install.cmd
+++ 01_install.cmd
@@ -2,6 +2,6 @@
 REM Create virtual environment
 python -m venv venv
 call venv\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+call python -m pip install --upgrade pip
+call pip install -r requirements.txt
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
